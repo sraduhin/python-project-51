672da3cfca336de85a967538e7502b5b83a9cc94
Sergey Radyukhin <22dollars@mail.ru>
2022-11-09 19:48:54 +0300

    parsing images

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -4,6 +4,8 @@ import re
 from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError
 
+VALID_EXTENSIONS = ['png', 'jpg']
+
 
 def get_file_name(url):
     host = url.split('//')[-1]
@@ -38,16 +40,31 @@ def download(url, dir):
             f.write(response.text)
             print('Success!')
             return download_path'''
-def parse_html(html_doc):
+
+def parse_img_tag(html_doc):
+    print(html_doc)
+    soup = BeautifulSoup(html_doc, 'html.parser')
+    img_links = []
+    for link in soup.find_all('img'):
+        img_links.append(link.get('src'))
+    print(filter(img_links, ))
     
+
+def download_html(html, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(html)
+        print('Success!')
+        return path
 
 def safe_pictures(pathfile):
     with open(pathfile, 'r', encoding='utf-8') as html_doc:
         html_doc = html_doc.read()
         soup = BeautifulSoup(html_doc, 'html.parser')
+        
 
 
 
 if __name__ == '__main__':
-    download('https://ru.hexlet.io/courses', 'var\\tmp')
-    safe_pictures('var\\tmp\\ru-hexlet-io-courses.html')
+    html = parse_http('https://ru.hexlet.io/courses', 'var\\tmp')
+    parse_img_tag(html)
+    # safe_pictures('var\\tmp\\ru-hexlet-io-courses.html')
