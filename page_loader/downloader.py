5b8e78acbdf884ae62e991c8907ead1d5f416a8d
Sergey Radyukhin <22dollars@mail.ru>
2022-11-16 08:06:32 +0300

    refactoring

--- page_loader/downloader.py
+++ page_loader/downloader.py
@@ -1,15 +1,13 @@
-import requests
 import os
-import re
+import requests
 import logging
+
 from bs4 import BeautifulSoup
 from requests.exceptions import HTTPError
-from urllib.parse import urlparse
 
-logging.basicConfig(filename='loader.log', level=logging.INFO)
+from page_loader.utils import get_host_and_create_local_name, localize_src, download_file, download_html, find_images, find_links, find_scripts
 
-VALID_IMG_EXTENSIONS = ['png', 'jpg']
-
+logging.basicConfig(level=logging.INFO, format="%(asctime)s %(levelname)s %(message)s")
 
 def main(url, dir):
     logging.info('loader has been started')
@@ -41,83 +39,13 @@ def main(url, dir):
             asset_copies = localize_src(origin_assets, local_dir)
             for num, path in enumerate(origin_assets):
                 download_file(path, asset_copies[num])
-                logging.info(f'success!')
         origins.extend(origin_assets)
         copies.extend(asset_copies)
         html = html_soup.prettify()
         for num, src in enumerate(origins):
             html = html.replace(src, copies[num])
-        
         return download_html(html, local_name)
 
 
-def get_host_and_create_local_name(url, dir=os.getcwd()):
-    obj = urlparse(url)
-    (name, ext) = os.path.splitext(obj.path)
-    ext = ext or '.html'
-    host = obj.hostname or ''
-    name = os.path.join(dir, normalize(host + name)) + ext
-    return host, name
-
-# ----------- utils
-def normalize(src):
-    parts = re.split(r';|_|-|/|\.', src)
-    return '-'.join(parts).strip('-')
-
-
-def find_images(html):
-    images = []
-    for link in html.find_all('img'):
-        src = link.get('src')
-        (_, extension) = os.path.splitext(src)
-        if extension[1:] in VALID_IMG_EXTENSIONS:
-            images.append(src)
-    return images
-
-
-def find_links(html, localhost):
-    links = []
-    for tagline in html.find_all('link'):
-        href = tagline.get('href')
-        originhost = urlparse(href).hostname
-        if not originhost or originhost == localhost:
-            links.append(href)
-    return links
-
-
-def find_scripts(html):
-    scripts = []
-    for tagline in html.find_all('script'):
-        src = tagline.get('src')
-        if src:
-            scripts.append(src)
-    return scripts
-        
-
-def localize_src(list, dir):
-    locals = []
-    for element in list:
-        _, name = get_host_and_create_local_name(element, dir)
-        locals.append(name)
-    return locals
-
-
-def download_file(origin_path, copy_path, images=False):
-    response = requests.get(origin_path)
-    if images:
-        with open(copy_path, 'wb') as copy:
-            copy.write(response.content)
-    else:
-        with open(copy_path, 'w', encoding='utf-8') as copy:
-            copy.write(response.text)
-
-
-def download_html(html, path):
-    with open(path, 'w', encoding='utf-8') as f:
-        f.write(html)
-        logging.info(f'html page has been successfully downloaded to {path}')
-        return path
-
-
 if __name__ == '__main__':
-    main('https://ru.hexlet.io/courses', 'var\\tmp')
+    main('https://ru.hexlet.io/3courses', 'var\\tmp')
---
+++ page_loader/utils.py
@@ -0,0 +1,75 @@
+import requests
+import os
+import re
+import logging
+from urllib.parse import urlparse
+
+VALID_IMG_EXTENSIONS = ['png', 'jpg']
+
+
+def get_host_and_create_local_name(url, dir=os.getcwd()):
+    obj = urlparse(url)
+    (name, ext) = os.path.splitext(obj.path)
+    ext = ext or '.html'
+    host = obj.hostname or ''
+    name = os.path.join(dir, normalize(host + name)) + ext
+    return host, name
+
+
+def normalize(src):
+    parts = re.split(r';|_|-|/|\.', src)
+    return '-'.join(parts).strip('-')
+
+
+def find_images(html):
+    images = []
+    for link in html.find_all('img'):
+        src = link.get('src')
+        (_, extension) = os.path.splitext(src)
+        if extension[1:] in VALID_IMG_EXTENSIONS:
+            images.append(src)
+    return images
+
+
+def find_links(html, localhost):
+    links = []
+    for tagline in html.find_all('link'):
+        href = tagline.get('href')
+        originhost = urlparse(href).hostname
+        if not originhost or originhost == localhost:
+            links.append(href)
+    return links
+
+
+def find_scripts(html):
+    scripts = []
+    for tagline in html.find_all('script'):
+        src = tagline.get('src')
+        if src:
+            scripts.append(src)
+    return scripts
+        
+
+def localize_src(list, dir):
+    locals = []
+    for element in list:
+        _, name = get_host_and_create_local_name(element, dir)
+        locals.append(name)
+    return locals
+
+
+def download_file(origin_path, copy_path, images=False):
+    response = requests.get(origin_path)
+    if images:
+        with open(copy_path, 'wb') as copy:
+            copy.write(response.content)
+    else:
+        with open(copy_path, 'w', encoding='utf-8') as copy:
+            copy.write(response.text)
+
+
+def download_html(html, path):
+    with open(path, 'w', encoding='utf-8') as f:
+        f.write(html)
+        logging.info(f'html page has been successfully downloaded to {path}')
+        return path
